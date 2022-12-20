a5e859bb3e38601afc5e1400a6086260bc66a0f6
Diana Linkova <di_rome@mail.ru>
2022-12-20 20:40:28 +0300

    completed task 3

--- main.js
+++ main.js
@@ -34,9 +34,15 @@ console.log(max(num3, num4));
 
 // Task 3
 
-// const array = [1, 30, 4, 0, 60, 5, 9, 3, 0, 4];
+const array = [1, 30, 4, 0, 60, 5, 9, 3, 20, 4];
 
-// function returnZeroArray(array2) {
-//   array.map();
-// }
-// console.log(returnZeroArray);
+const returnZeroArray = array.map(function (element) {
+  if (element == 0) {
+    return 'zero';
+  } else if (element % 10 == 0) {
+    return element.toString().replace(0, 'zero');
+  }
+  return element;
+});
+
+console.log(returnZeroArray);
