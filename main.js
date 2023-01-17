c62914c538afa715935e51b2727b02c889a246bf
Diana Linkova <di_rome@mail.ru>
2023-01-17 16:36:55 +0300

    create palindrome

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-// Task 1
-function palindrom(word) {
-  return word.toLowerCase() == word.toLowerCase().split('').reverse().join('');
-}
-
-// Task 2.1
-function min(a, b) {
-  return a < b ? a : b;
-}
-
-// Task 2.2
-function max(x, y) {
-  return x > y ? x : y;
-}
-
-// Task 3
-
-const array = [1, 30, 4, 0, 60, 5, 9, 3, 200, 4];
-
-const returnZeroArray = array.map(function (element) {
-  if (element == 0) {
-    return 'zero';
-  } else if (element % 10 == 0) {
-    return element.toString().replaceAll(0, 'zero');
-  }
-  return element;
-});
+const palindrome = (str) => {
+  return str === str.split('').reverse().join('');
+};
+console.log(palindrome('sos'));
