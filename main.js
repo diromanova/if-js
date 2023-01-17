7dd14f8dd87a10480405c36e3c08037abe397a0e
Diana Linkova <di_rome@mail.ru>
2023-01-17 16:24:02 +0300

    completed task2

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ function sum(numOne) {
 }
 
 console.log(sum(5)(2));
+
+const textOne = document.getElementById('text1');
+const textTwo = document.getElementById('text2');
+const textThree = document.getElementById('text3');
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+function colorChange() {
+  let i = 0;
+  return function (e) {
+    e.target.style.color = colors[i];
+    i++;
+    if (i >= colors.length) {
+      i = 0;
+    }
+  };
+}
+
+textOne.addEventListener('click', colorChange());
+textTwo.addEventListener('click', colorChange());
+textThree.addEventListener('click', colorChange());
